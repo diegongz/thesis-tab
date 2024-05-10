9acec0b53257938599db44114e995bf9df067413
Diego <diego.ngzru@gmail.com>
2024-05-09 21:50:12 -0600

    Plots_functions

---
+++ utils/plots.py
@@ -0,0 +1,48 @@
+import matplotlib.pyplot as plt
+import skorch
+
+def line_plot(x, y, z, label_1, label_2):
+    # Create a new figure
+    fig, ax = plt.subplots()
+
+    # Plot x vs y with a blue line
+    ax.plot(x, y, color='blue', label= label_1)
+
+    # Plot x vs z with a red line
+    ax.plot(x, z, color='red', label=label_2)
+
+    # Add legends for each plot
+    ax.legend(loc='upper left')
+
+    # Return the plot object
+    return fig
+
+def model_plots(model):
+    epochs = []
+
+    train_acc = []
+    val_acc = []
+
+    train_loss = []
+    val_loss = []
+
+
+    for x in model.history:
+        epoch_num = x["epoch"]
+        epochs.append(epoch_num)
+
+        train_acc.append(x["accuracy"])
+        val_acc.append(x['valid_acc'])
+
+        train_loss.append(x["train_loss"])
+        val_loss.append(x["valid_loss"])
+
+    fig_1 = line_plot(epochs, train_acc, val_acc, "train_acc", "val_acc")
+    fig_2 = line_plot(epochs,train_loss,val_loss, "train_loss", "val_loss")
+    
+    plt.show(fig_1)
+    plt.show(fig_2)
+
+
+    
+
