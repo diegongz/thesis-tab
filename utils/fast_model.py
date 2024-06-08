df72617a6fba5ec61491c5a4fb0bb2c80a8db25b
Diego <diego.ngzru@gmail.com>
2024-06-08 14:45:15 -0600

    Except function in trainning

--- utils/fast_model.py
+++ utils/fast_model.py
@@ -92,179 +92,189 @@ def train_model(task_id, n_layers_lst, n_heads_lst, embed_dim, batch_size, epoch
   
 
     #create the folder to save the dataset experiments if it doesn't exist
-    new_folder(project_path, "data_models")
-    path_of_data_models = os.path.join(project_path, "data_models") #path of the folder data_models
+    new_folder(project_path, "parameter_selection_models")
+    path_of_data_models = os.path.join(project_path, "parameter_selection_models") #path of the folder data_models
 
     #create the folder for specific dataset name
-    new_folder(path_of_data_models, f"{dataset_name}_{embed_dim}")
-    path_of_dataset = os.path.join(path_of_data_models, f"{dataset_name}_{embed_dim}") #path of the folder dataset 
+    new_folder(path_of_data_models, f"{dataset_name}") #_{embed_dim}") #path of the folder dataset
+    path_of_dataset = os.path.join(path_of_data_models, f"{dataset_name}") #path of the folder dataset 
 
     #save a .txt in the folder to save the validation indices
     np.savetxt(os.path.join(path_of_dataset, "validation_indices"), val_indices)
     
     experiment_num = 1
     
-    for n_layer in n_layers_lst:
-        for n_head in n_heads_lst:
-            
-            #parameters
-            n_layers = n_layer
-            n_heads = n_head
-            embed_dim = embed_dim #The embedding size is set one by one to avoid the out of memory error
-            batch_size = batch_size # 32, 64, 128, 256, 512, 1024
-            epochs = epochs
+    for embd in embed_dim:
+        for n_layer in n_layers_lst:
+            for n_head in n_heads_lst:
+                try: 
+                    print("--------------------------------------------------------------")
+                    print(f"NAME: {dataset_name}")
+                    print(f"Embedding Size: {embd}")
+                    print(f"Number of Layers: {n_layer}")
+                    print(f"Number of Heads: {n_head}")
 
-            #parameters for the model
-            ff_pw_size = 30  #this value because of the paper 
-            attn_dropout = 0.3 #paper
-            ff_dropout = 0.1 #paper value
-            aggregator = "cls"
-            aggregator_parameters = None
-            decoder_hidden_units = [128,64] #paper value [128,64]
-            decoder_activation_fn = nn.ReLU()
-            need_weights = False
-            numerical_passthrough = False       
-            
-            print("----------------------------------------------------------------------")
-            print("New model:")
+                    #parameters
+                    n_layers = n_layer
+                    n_heads = n_head
+                    embedding_size = embd #The embedding size is set one by one to avoid the out of memory error
+                    batch_size = batch_size # 32, 64, 128, 256, 512, 1024
+                    epochs = epochs
 
-            #experiment i folder
-            new_folder(path_of_dataset, f"experiment_{experiment_num}")
-            path_of_experiment = os.path.join(path_of_dataset, f"experiment_{experiment_num}") #In this folder it will be saved the model and images
+                    #parameters for the model
+                    ff_pw_size = 30  #this value because of the paper 
+                    attn_dropout = 0.3 #paper
+                    ff_dropout = 0.1 #paper value
+                    aggregator = "cls"
+                    aggregator_parameters = None
+                    decoder_hidden_units = [128,64] #paper value [128,64]
+                    decoder_activation_fn = nn.ReLU()
+                    need_weights = False
+                    numerical_passthrough = False       
+                    
+                    #experiment i folder
+                    new_folder(path_of_dataset, f"experiment_{experiment_num}_{embedding_size}_{n_layer}_{n_head}")
+                    path_of_experiment = os.path.join(path_of_dataset, f"experiment_{experiment_num}_{embedding_size}_{n_layer}_{n_head}") #In this folder it will be saved the model and images
 
-            #create the folder for the checkpoints
-            new_folder(path_of_experiment, "checkpoints")
-            path_of_checkpoint = os.path.join(path_of_experiment, "checkpoints") #path to save the checkpoints
+                    #create the folder for the checkpoints
+                    new_folder(path_of_experiment, "checkpoints")
+                    path_of_checkpoint = os.path.join(path_of_experiment, "checkpoints") #path to save the checkpoints
 
-            #create the folder for the plots
-            new_folder(path_of_experiment, "plots")
-            path_of_plots = os.path.join(path_of_experiment, "plots") #path of the folder dataset
+                    #create the folder for the plots
+                    new_folder(path_of_experiment, "plots")
+                    path_of_plots = os.path.join(path_of_experiment, "plots") #path of the folder dataset
 
-            #module
-            module = training.build_module(
-                n_categories, # List of number of categories
-                n_numerical, # Number of numerical features
-                n_heads, # Number of heads per layer
-                ff_pw_size, # Size of the MLP inside each transformer encoder layer
-                n_layers, # Number of transformer encoder layers    
-                n_labels, # Number of output neurons
-                embed_dim,
-                attn_dropout, 
-                ff_dropout, 
-                aggregator, # The aggregator for output vectors before decoder
-                rnn_aggregator_parameters=aggregator_parameters,
-                decoder_hidden_units=decoder_hidden_units,
-                decoder_activation_fn=decoder_activation_fn,
-                need_weights=need_weights,
-                numerical_passthrough=numerical_passthrough
-            ) 
+                    #module
+                    module = training.build_module(
+                        n_categories, # List of number of categories
+                        n_numerical, # Number of numerical features
+                        n_heads, # Number of heads per layer
+                        ff_pw_size, # Size of the MLP inside each transformer encoder layer
+                        n_layers, # Number of transformer encoder layers    
+                        n_labels, # Number of output neurons
+                        embedding_size,
+                        attn_dropout, 
+                        ff_dropout, 
+                        aggregator, # The aggregator for output vectors before decoder
+                        rnn_aggregator_parameters=aggregator_parameters,
+                        decoder_hidden_units=decoder_hidden_units,
+                        decoder_activation_fn=decoder_activation_fn,
+                        need_weights=need_weights,
+                        numerical_passthrough=numerical_passthrough
+                    ) 
 
-            #MODEL
-            model = skorch.NeuralNetClassifier(
-                module=module,
-                criterion=torch.nn.CrossEntropyLoss,
-                optimizer=torch.optim.AdamW,
-                device = "cuda" if torch.cuda.is_available() else "cpu",
-                batch_size = batch_size,
-                max_epochs = epochs,
-                train_split=skorch.dataset.ValidSplit(((train_indices, val_indices),)),
-                callbacks=[
-                    ("balanced_accuracy", skorch.callbacks.EpochScoring("balanced_accuracy", lower_is_better=False)),
-                    ("duration", skorch.callbacks.EpochTimer()),
-                    EpochScoring(scoring='accuracy', name='train_acc', on_train=True), #
-                    Checkpoint(dirname = path_of_checkpoint, load_best = True), 
-                    EarlyStopping(patience=15)
+                    #MODEL
+                    model = skorch.NeuralNetClassifier(
+                        module=module,
+                        criterion=torch.nn.CrossEntropyLoss,
+                        optimizer=torch.optim.AdamW,
+                        device = "cuda" if torch.cuda.is_available() else "cpu",
+                        batch_size = batch_size,
+                        max_epochs = epochs,
+                        train_split=skorch.dataset.ValidSplit(((train_indices, val_indices),)),
+                        callbacks=[
+                            ("balanced_accuracy", skorch.callbacks.EpochScoring("balanced_accuracy", lower_is_better=False)),
+                            ("duration", skorch.callbacks.EpochTimer()),
+                            EpochScoring(scoring='accuracy', name='train_acc', on_train=True), #
+                            Checkpoint(dirname = path_of_checkpoint, load_best = True), 
+                            EarlyStopping(patience=20)
 
-                ],
-                optimizer__lr=1e-4,
-                optimizer__weight_decay=1e-4
-            )
-            ''' 
-            model = skorch.NeuralNetClassifier(
-            module=module,
-            criterion=torch.nn.CrossEntropyLoss,
-            optimizer=torch.optim.AdamW,
-            device="cuda" if torch.cuda.is_available() else "cpu",
-            batch_size = batch_size,
-            max_epochs = epochs,
-            train_split=skorch.dataset.ValidSplit(((train_indices, val_indices),)),
-            callbacks=[
-                ("balanced_accuracy", skorch.callbacks.EpochScoring("balanced_accuracy", lower_is_better=False)),
-                ("accuracy", skorch.callbacks.EpochScoring("accuracy", lower_is_better=False)),
-                ("duration", skorch.callbacks.EpochTimer()),
-                EpochScoring(scoring='accuracy', name='train_acc', on_train=True)
-            ],
-            optimizer__lr=0.5,
-            optimizer__weight_decay=1e-4
-            )
-            
-             
-            model = skorch.NeuralNetClassifier(
-                module=module,
-                criterion=torch.nn.CrossEntropyLoss,
-                optimizer=torch.optim.AdamW,
-                device = "cpu", #if torch.cuda.is_available() else "cpu",
-                batch_size = batch_size,
-                max_epochs = epochs,
-                train_split=skorch.dataset.ValidSplit(((train_indices, val_indices),)),
-                callbacks=[
-                    ("balanced_accuracy", skorch.callbacks.EpochScoring("balanced_accuracy", lower_is_better=False)),
-                    ("duration", skorch.callbacks.EpochTimer()),
-                    ("accuracy", skorch.callbacks.EpochScoring("accuracy", lower_is_better=False)),
-                    EpochScoring(scoring='accuracy', name='train_acc', on_train=True), # Compute accuracy at the end of each epoch on the training set
-                    Checkpoint(dirname= path_of_checkpoint,load_best = True),
-                    EarlyStopping(patience=20)
-
-                ],
-                optimizer__lr=1e-4,
-                optimizer__weight_decay=1e-4
-            )
-            '''
-            
-            # Define Checkpoint and TrainEndCheckpoint callbacks with custom directory
-            cp = Checkpoint()
-            train_end_cp = TrainEndCheckpoint()
-
-            start_time = time.time()  # Start the timer to count how long does it takes
-            #TRAINING
-            model = model.fit(X={
-                    "x_numerical": X_train[:, :n_numerical].astype(np.float32),
-                    "x_categorical": X_train[:, n_numerical:].astype(np.int32)
-                    }, 
-                    y=y_train.astype(np.int64)
+                        ],
+                        optimizer__lr=1e-4,
+                        optimizer__weight_decay=1e-4
                     )
-            end_time = time.time()  # Stop the timer
-            training_time = end_time - start_time  # Calculate the elapsed time
-            #TESTING
-            predictions = model.predict_proba(X={
-                            "x_numerical": X_test[:, :n_numerical].astype(np.float32),
-                            "x_categorical": X_test[:, n_numerical:].astype(np.int32)
-                            }
+                    ''' 
+                    model = skorch.NeuralNetClassifier(
+                    module=module,
+                    criterion=torch.nn.CrossEntropyLoss,
+                    optimizer=torch.optim.AdamW,
+                    device="cuda" if torch.cuda.is_available() else "cpu",
+                    batch_size = batch_size,
+                    max_epochs = epochs,
+                    train_split=skorch.dataset.ValidSplit(((train_indices, val_indices),)),
+                    callbacks=[
+                        ("balanced_accuracy", skorch.callbacks.EpochScoring("balanced_accuracy", lower_is_better=False)),
+                        ("accuracy", skorch.callbacks.EpochScoring("accuracy", lower_is_better=False)),
+                        ("duration", skorch.callbacks.EpochTimer()),
+                        EpochScoring(scoring='accuracy', name='train_acc', on_train=True)
+                    ],
+                    optimizer__lr=0.5,
+                    optimizer__weight_decay=1e-4
+                    )
+                    
+                    
+                    model = skorch.NeuralNetClassifier(
+                        module=module,
+                        criterion=torch.nn.CrossEntropyLoss,
+                        optimizer=torch.optim.AdamW,
+                        device = "cpu", #if torch.cuda.is_available() else "cpu",
+                        batch_size = batch_size,
+                        max_epochs = epochs,
+                        train_split=skorch.dataset.ValidSplit(((train_indices, val_indices),)),
+                        callbacks=[
+                            ("balanced_accuracy", skorch.callbacks.EpochScoring("balanced_accuracy", lower_is_better=False)),
+                            ("duration", skorch.callbacks.EpochTimer()),
+                            ("accuracy", skorch.callbacks.EpochScoring("accuracy", lower_is_better=False)),
+                            EpochScoring(scoring='accuracy', name='train_acc', on_train=True), # Compute accuracy at the end of each epoch on the training set
+                            Checkpoint(dirname= path_of_checkpoint,load_best = True),
+                            EarlyStopping(patience=20)
+
+                        ],
+                        optimizer__lr=1e-4,
+                        optimizer__weight_decay=1e-4
+                    )
+                    '''
+                    
+                    # Define Checkpoint and TrainEndCheckpoint callbacks with custom directory
+                    cp = Checkpoint()
+                    train_end_cp = TrainEndCheckpoint()
+
+                    start_time = time.time()  # Start the timer to count how long does it takes
+                    #TRAINING
+                    model = model.fit(X={
+                            "x_numerical": X_train[:, :n_numerical].astype(np.float32),
+                            "x_categorical": X_train[:, n_numerical:].astype(np.int32)
+                            }, 
+                            y=y_train.astype(np.int64)
                             )
-            
-            print("Test results:\n")
-            print(evaluating.get_default_scores(y_test.astype(np.int64), predictions, multiclass = multiclass_val))
-            
-            balanced_accuracy = evaluating.get_default_scores(y_test, predictions, multiclass = multiclass_val)["balanced_accuracy"]
-            accuracy = evaluating.get_default_scores(y_test, predictions, multiclass = multiclass_val)["accuracy"]
-            log_loss = evaluating.get_default_scores(y_test, predictions, multiclass = multiclass_val)["log_loss"]
-            max_epochs = len(model.history)
+                    end_time = time.time()  # Stop the timer
+                    training_time = end_time - start_time  # Calculate the elapsed time
+                    
+                    #TESTING
+                    #I will use only the valdation indices to have the model selection 
 
-            #save the results in a list
-            result_row = [dataset_name, experiment_num, n_layers, n_heads, embed_dim, batch_size, balanced_accuracy, accuracy, log_loss, max_epochs, training_time]
-            results_table.append(result_row)
 
-            #create and save the plots
-            fig_1, fig_2 = plots.model_plots(model, f"Experiment {experiment_num}")
+                    predictions = model.predict_proba(X={
+                                    "x_numerical": X_train[:, :n_numerical].astype(np.float32),
+                                    "x_categorical": X_train[:, n_numerical:].astype(np.int32)
+                                    }
+                                    )
+                    
+                    print("Test results in validation:\n")
+                    print(evaluating.get_default_scores(y_train[val_indices].astype(np.int64), predictions[val_indices], multiclass = multiclass_val))
+                    
+                    balanced_accuracy = evaluating.get_default_scores(y_train[val_indices].astype(np.int64), predictions[val_indices], multiclass = multiclass_val)["balanced_accuracy"]
+                    accuracy = evaluating.get_default_scores(y_train[val_indices].astype(np.int64), predictions[val_indices], multiclass = multiclass_val)["accuracy"]
+                    log_loss = evaluating.get_default_scores(y_train[val_indices].astype(np.int64), predictions[val_indices], multiclass = multiclass_val)["log_loss"]
+                    max_epochs = len(model.history)
 
-            # Save the first figure
-            fig_1.savefig(os.path.join(path_of_plots, 'figure1.png'))
+                    #save the results in a list
+                    result_row = [dataset_name, experiment_num, n_layers, n_heads, embed_dim, batch_size, balanced_accuracy, accuracy, log_loss, max_epochs, training_time]
+                    results_table.append(result_row)
 
-            # Save the second figure
-            fig_2.savefig(os.path.join(path_of_plots, 'figure2.png'))
+                    #create and save the plots
+                    fig = plots.model_plots(model, f"Experiment {experiment_num}")
 
-            #increase experiment number, this one is used for the plots
-            experiment_num += 1
+                    # Save the first figure
+                    fig.savefig(os.path.join(path_of_plots, 'plots.png'))
+                    #increase experiment number, this one is used for the plots
+                    
+                    experiment_num += 1
+
+                except RuntimeError as e:
+                    if 'out of memory' in str(e):
+                        print(f"CUDA out of memory")
+                        torch.cuda.empty_cache()  # Free up memory if possible               
 
 
     export_to_csv(results_table, columns_names, path_of_dataset)
--- utils/plots.py
+++ utils/plots.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import skorch
-
+''' 
 def line_plot(x, y, z, label_1, label_2, title):
     # Create a new figure
     fig, ax = plt.subplots()
@@ -40,8 +40,8 @@ def model_plots(model, title):
         train_loss.append(x["train_loss"])
         val_loss.append(x["valid_loss"])
 
-    fig_1 = line_plot(epochs, train_acc, val_acc, "train_acc", "val_acc", title)
-    fig_2 = line_plot(epochs,train_loss,val_loss, "train_loss", "val_loss", title)
+    fig_1 = line_plot(epochs, train_acc, val_acc, "train_acc", "val_acc", f"{title}_Accuracy")
+    fig_2 = line_plot(epochs,train_loss,val_loss, "train_loss", "val_loss", f"{title}_Loss")
     
     #plt.show(fig_1)
     #plt.show(fig_2)
@@ -50,3 +50,49 @@ def model_plots(model, title):
 
     
 
+'''
+
+def line_plot(ax, x, y, z, label_1, label_2, title):
+    # Plot x vs y with a blue line
+    ax.plot(x, y, color='blue', label=label_1)
+
+    # Plot x vs z with a red line
+    ax.plot(x, z, color='red', label=label_2)
+
+    # Add legends for each plot
+    ax.legend(loc='upper left')
+
+    # Set the title
+    ax.set_title(title)
+
+def model_plots(model, title):
+    epochs = []
+
+    train_acc = []
+    val_acc = []
+
+    train_loss = []
+    val_loss = []
+
+    for x in model.history:
+        epoch_num = x["epoch"]
+        epochs.append(epoch_num)
+
+        train_acc.append(x['train_acc'])
+        val_acc.append(x['valid_acc'])
+
+        train_loss.append(x["train_loss"])
+        val_loss.append(x["valid_loss"])
+
+    # Create a figure with 1 row and 2 columns
+    fig, axs = plt.subplots(1, 2, figsize=(15, 5))
+
+    # Plot the accuracy and loss on the respective subplots
+    line_plot(axs[0], epochs, train_acc, val_acc, "train_acc", "val_acc", f"{title}_Accuracy")
+    line_plot(axs[1], epochs, train_loss, val_loss, "train_loss", "val_loss", f"{title}_Loss")
+
+    # Adjust layout to prevent overlap
+    plt.tight_layout()
+
+    # Return the combined figure
+    return fig
