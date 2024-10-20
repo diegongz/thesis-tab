b1e519b6dad85ffd6e9a00b8f6d1ae7100d5c18a
Diego <diego.ngzru@gmail.com>
2024-10-19 22:23:19 -0600

    XGBoost BayesSearch All

--- experiments/finals/xgboost/train_xgboost_bayes.py
+++ experiments/finals/xgboost/train_xgboost_bayes.py
@@ -16,7 +16,7 @@ from itertools import product
 import time
 from skopt import BayesSearchCV
 from skopt.space import Real, Integer
-import xgboost as xgb
+import numpy as np
 
 
 
@@ -50,7 +50,7 @@ param_grid = {
 
 
 df_id = 31
-sample_size = [100]
+sample_size = [100,80,60,40,20]
 seed = 11
 
 name_df = data.get_dataset_name(df_id)
@@ -60,6 +60,9 @@ os.makedirs(path_of_datset, exist_ok=True)
 
 path_of_xgboost = f'{path_of_datset}/xgboost'
 os.makedirs(path_of_xgboost, exist_ok=True)
+
+path_hyperparameter_selection = f'{path_of_xgboost}/hyperparameter_selection'
+os.makedirs(path_hyperparameter_selection, exist_ok=True)
 
 
 # Define parameter space for Bayesian optimization
@@ -75,7 +78,6 @@ search_space = {
 
 for sample in sample_size:
     if sample == 100:
-        path_hyperparameter_selection = f'{path_of_xgboost}/hyperparameter_selection'
         #create the folder of the sample size
         size_path_hyper = f'{path_hyperparameter_selection}/{sample}'
         os.makedirs(size_path_hyper, exist_ok=True)
@@ -109,9 +111,95 @@ for sample in sample_size:
 
         df_results = pd.DataFrame(results, index=[0])
         df_results.to_csv(f'{path_of_final_xgboost}/results.csv', index=False)
+    
+    else:
+        # Set the seed for reproducibility
+        np.random.seed(seed)
+
+        # Generate 10 random integers between a specified range (e.g., 0 to 100)
+        random_seeds = np.random.randint(0, 100, size=10)
+
+        number_of_seed = 1
+
+        for random_seed in random_seeds:
+            X_train, X_test, y_train, y_test, train_indices, val_indices, n_instances, n_labels, n_numerical, n_categories = data.import_data(df_id)
+
+            seed_folder = f'{path_hyperparameter_selection}/{sample}/trials/seed_{number_of_seed}'
+            os.makedirs(seed_folder, exist_ok=True)
+
+            #create a random sample of instances 
+            _, _, general_n_percent = data.reduce_size(y_train, train_indices, val_indices, sample, random_seed)
+
+            #create the new X_train and y_train with the reduced size, this sets are the ones that will enter to the 5 fold cross validation
+            X_train = X_train[general_n_percent]
+            y_train = y_train[general_n_percent]
+
+            best_model, best_params, cv_results, best_score = xgboost_file.xgboost_bayesian(search_space, X_train, y_train, n_labels)
+
+            results_param_selection = best_params.copy()
+
+            # update the best parameters with the best score
+            results_param_selection['balanced_accuracy_mean'] = best_score
+
+            df_param = pd.DataFrame(results_param_selection, index=[0])
+            df_param.to_csv(f'{seed_folder}/results.csv', index=False)
+
+            # Save the cross-validation results
+            cv_results = pd.DataFrame(cv_results)
+            cv_results.to_csv(f'{seed_folder}/cv_results.csv', index=False)
+
+            # Evaluate the best model on the test set
+            results = xgboost_file.evaluate_xgboost_bayesian(best_model, X_train, y_train, X_test, y_test)
+
+            results.update(best_params)
+
+#----------------------------------------------------------------------------------------------------
+            #Save results
+            path_of_final_xgboost = f'{path_of_datset}/xgboost/final_xgboost/{sample}/trials/seed_{number_of_seed}'
+            os.makedirs(path_of_final_xgboost, exist_ok=True)
+
+            df_results = pd.DataFrame(results, index=[0])
+            df_results.to_csv(f'{path_of_final_xgboost}/results.csv', index=False)
+
+            number_of_seed += 1
 
 
+        #Obtain the means and the std deviation of all the results
+        path_of_trials = f'{path_of_datset}/xgboost/final_xgboost/{sample}/trials'
+        
+        all_data = []
 
+        # Iterate over each subfolder in final_tab_size_path
+        for subfolder in os.listdir(path_of_trials):
+            subfolder_path = os.path.join(path_of_trials, subfolder)
+
+            results_file = os.path.join(subfolder_path, 'results.csv')
+
+            df = pd.read_csv(results_file)
+            all_data.append(df)
+
+            combined_results = pd.concat(all_data, ignore_index=True)
+
+        # Now calculate the mean and std for columns 1 to 8
+        mean_values = combined_results.iloc[:, :5].mean()
+        std_values = combined_results.iloc[:, :5].std()
+
+
+        # Create a dictionary with the new column names
+        mean_columns = {col: f"{col}_mean" for col in combined_results.columns[:5]}
+        std_columns = {col: f"{col}_std" for col in combined_results.columns[:5]}
+
+
+        # Create a new DataFrame with the results, renaming columns accordingly
+        summary_df = pd.DataFrame({
+            **{mean_columns[col]: [mean_values[col]] for col in combined_results.columns[:5]},
+            **{std_columns[col]: [std_values[col]] for col in combined_results.columns[:5]}
+        })
+
+        # Optionally, you can reset the index to avoid having index numbers in the final result
+        summary_df.reset_index(drop=True, inplace=True)
+
+        summary_df.to_csv(f'{path_of_trials}/results.csv', index=False)
 
 
 
